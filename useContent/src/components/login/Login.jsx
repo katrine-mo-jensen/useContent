49d848a95a90c23b3e3b5ab31aa836b39885e2d3
katrine-mo-jensen <katrine.mo.jensen@gmail.com>
2023-09-06 13:15:44 +0200

    posts

--- useContent/src/components/login/Login.jsx
+++ useContent/src/components/login/Login.jsx
@@ -2,68 +2,58 @@ import { useContext } from "react";
 import { UserContext } from "../../context/UserContext";
 
 export const Login = () => {
+  const { setUser, user } = useContext(UserContext);
 
-    const {setUser,user} = useContext(UserContext);
+  const submitHandler = (event) => {
+    event.preventDefault();
 
+    const username = event.target.username.value;
+    const password = event.target.password.value;
 
-    const submitHandler = (event) => {
-        event.preventDefault()
+    if (username !== "" && password !== "") {
+      const url = "https://dummyjson.com/auth/login";
+      const options = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          username: username,
+          password: password,
+        }),
+      };
 
-        const username = event.target.username.value
-        const password = event.target.password.value
+      fetch(url, options)
+        .then((res) => res.json())
+        // .then((data) => console.log(data))
+        .then((data) => setUser(data));
 
+      // .then((err) => console.error(err));
+    } else {
+      alert("du skal udfylde felterne fær du sender!");
+    }
 
-if (username !== "" &&  password !== ""  ) {
+    console.log("username:", event.target.username.value);
+    console.log("password:", event.target.password.value);
+  };
 
-    const url = 'https://dummyjson.com/auth/login';
-    const options = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      
-      username: username,
-      password:  password,
-    })}
-
-    fetch (url, options)
-    .then((res) => res.json())
-    // .then((data) => console.log(data))
-    .then((data) => setUser(data))
-
-    // .then((err) => console.error(err));
-}
-else {
-    alert('du skal udfylde felterne fær du sender!')
-}
-
-        console.log("username:", event.target.username.value)
-        console.log("password:", event.target.password.value)
-    };
-
-    if (user?.firstName)
-
+  if (user?.firstName)
     return (
-        <>
-         <h3>Velkommen {user.username} </h3>
-          <button onClick={() => setUser(null)}>Log ud</button>
-        </>
-
-    )
-else 
+      <>
+        <h3>Velkommen {user.username} </h3>
+        <button onClick={() => setUser(null)}>Log ud</button>
+      </>
+    );
+  else
     return (
-
-        <form onSubmit={(event) => submitHandler(event)}>
-            <label>
-                Brugernavn:
-
-            <input name="username" placeholder="indtast brugernavn"/>
-            </label>
-            <label>
-
-             Password: 
-<input name="password" placeholder="intast password"/>
-            </label>
-            <input type="submit" value="send"/>
-        </form>
-    )
-}
+      <form onSubmit={(event) => submitHandler(event)}>
+        <label>
+          Brugernavn:
+          <input name="username" placeholder="indtast brugernavn" />
+        </label>
+        <label>
+          Password:
+          <input name="password" placeholder="intast password" />
+        </label>
+        <input type="submit" value="send" />
+      </form>
+    );
+};
--- useContent/src/pages/UserPostsPage.jsx
+++ useContent/src/pages/UserPostsPage.jsx
@@ -3,46 +3,63 @@ import { UserContext } from "../context/UserContext";
 import { useNavigate } from "react-router-dom";
 
 export const UserPostsPage = () => {
+  //lav et fetch ,ed brugerens id for at få de posts som deres brugere har lavet
+
   const { user } = useContext(UserContext);
   const [errorMsg, setErrorMsg] = useState("");
+  const [userPosts, setUserPosts] = useState(null);
 
   const navigate = useNavigate();
 
   useEffect(() => {
+    //tjek først om user findes
     if (user) {
       const id = user.id;
       const token = user.token;
+
       const url = `https://dummyjson.com/users/${id}/posts`;
-      console.log("User er logget ind");
+      console.log("user er logget ind");
 
       const options = {
-        metode: "GET",
+        method: "GET",
         headers: {
           Authorization: `Bearer ${token}`,
-          "Content-type": "application/json",
+          "Content-Type": "application/json",
         },
       };
+
       fetch(url, options)
         .then((res) => res.json())
         .then((data) => setUserPosts(data))
-        .catch((err) => console.log(err));
+        .catch((err) => console.error(err));
     } else {
-      console.log("User er ikke logget ind");
+      console.log("user er ikke logget ind!");
       setErrorMsg(
-        "Du skal være logget ind, for at kunne se posts. Du vil blive viderestillet til login"
+        "Du skal være logget ind for at se posts. Du vil blive sendt tilbage til login"
       );
 
-      let timeOut = setTimeout(() => {
+      const timeout = setTimeout(() => {
         navigate("/login");
       }, 5000);
-      return () => clearTimeout(timeOut);
+
+      return () => clearTimeout(timeout);
     }
   }, []);
 
+  console.log("UserPosts", userPosts);
+
   return (
     <section>
-      <h2>Posts</h2>
+      <h3>posts</h3>
       <h4>{errorMsg}</h4>
+      {userPosts?.posts.map((item) => {
+        return (
+          <div key={item.id}>
+            <p>{item.title}</p>
+            <p>{item.body}</p>
+          </div>
+        );
+      })}
     </section>
   );
 };
