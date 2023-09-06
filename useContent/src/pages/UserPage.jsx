787a47d2a7e2554bc5c668ea0589bdd516a7de87
katrine-mo-jensen <katrine.mo.jensen@gmail.com>
2023-09-06 13:36:38 +0200

    userpage more

--- useContent/src/pages/UserPage.jsx
+++ useContent/src/pages/UserPage.jsx
@@ -1,32 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useContext } from "react";
 import { CountButton } from "../components/CountButton/CountButton";
 import { ContentSection } from "../components/contentSection/ContentSection";
+import { UserContext } from "../context/UserContext";
 
-export const UserPage = () => {
+export const UserPage = (props) => {
   // Eksempel der viser Mount / unmount logik
-  useEffect(() => {
-    console.log("Blog Page Component has Mounted");
-    return () => {
-      console.log("Blog Page Component has Un-Mounted");
-    };
-  }, []);
+  const { user } = useContext(UserContext);
 
   return (
-    <ContentSection title="User page">
-      <h3>Here is the Blog page</h3>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
-        numquam, assumenda eveniet autem accusamus at, adipisci officia
-        repellendus sapiente ipsa aperiam cumque similique! Tenetur, quis odit
-        repellendus voluptatem qui ipsa.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
-        numquam, assumenda eveniet autem accusamus at, adipisci officia
-        repellendus sapiente ipsa aperiam cumque similique! Tenetur, quis odit
-        repellendus voluptatem qui ipsa.
-      </p>
-
+    <ContentSection title={props.title}>
+      {user ? (
+        <div>
+          <h3>
+            Hej {user?.firstName} {user?.lastname}
+          </h3>
+          <p>Din email: {user?.email}</p>
+          <p>Dit brugernavn: {user?.username}</p>
+        </div>
+      ) : (
+        <h3>Du skal være logget ind</h3>
+      )}
       <CountButton />
     </ContentSection>
   );
